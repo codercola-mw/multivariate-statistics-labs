35862a34f9c04d3e3f05ab1f565f50ea538fc58d
ahmetakdeve <55084294+ahmetakdeve@users.noreply.github.com>
2019-11-20 13:19:48 +0100

    Q2B Q2C Q3D

--- AA_code.R
+++ AA_code.R
@@ -3,19 +3,6 @@
 data <- read.delim(file="C:\\Users\\Suat\\Desktop\\Master_courses\\732A97_Multivariate\\Materials\\T1-9.dat", sep="\t", header=FALSE)
 colnames(data)<-c("Country","100m","200m","400m","800m","1500m","3000m","Marathon")
 
-##Q1. a
-charac <- data.frame(variable = as.numeric(), mean=as.numeric(), std_dev=as.numeric(), stringsAsFactors = FALSE)
-for (i in 2:length(data)) {
-  mean_v <- mean(data[[i]])
-  std_dev_v <- sd(data[[i]])
-  charac <- rbind(charac, data.frame(variable = as.numeric(i-1), mean=mean_v, std_dev=std_dev_v))
-}
-charac
-
-
-##Q1.b
-
-
 ##Q2. a
 data_mat <- as.matrix(data[,-1])
 for (i in 1:7) {
@@ -27,7 +14,7 @@ for (i in 1:nrow(data_mat)) {
   mean_mat[i,] <- mean_vect
 }
 mean_correct_mat <- data_mat - mean_mat
-cov_mat <- t(mean_correct_mat) %*% mean_correct_mat
+#cov_mat <- t(mean_correct_mat) %*% mean_correct_mat
 
 cov_mat<-cov(data[,-1])
 cor_mat<-cor(data[,-1])
@@ -45,40 +32,38 @@ faces(data[,-1],labels = data[,1])
 #Pairwise correlation between the variables. Plot.
 #install.packages("corrplot")
 library(corrplot)
-corrplot(cor_mat) #Very high correlations between the variables
+corrplot(cor_mat) #Very high correlations between all the variables
 
 
 #Multivariate residuals plot. Outliers can be detected from the plots here
-mean_correct_mat<-as.data.frame(mean_correct_mat)
-mean_correct_mat$country<-data$Country
-rownames(mean_correct_mat)<-data[,1]
-colnames(mean_correct_mat)<-c(colnames(data[,-1]),"country")
+mean_correct_mat2<-as.data.frame(mean_correct_mat)
+mean_correct_mat2$country<-data$Country
+rownames(mean_correct_mat2)<-data[,1]
+colnames(mean_correct_mat2)<-c(colnames(data[,-1]),"country")
 
-plot(x="country",y="100m",data=mean_correct_mat)
-
+#With Loop. Use this one!
 par(mfrow=c(3,3))
-plot(x=1:54,y=mean_correct_mat$`100m`,xlab = "Country",ylab="100m")
-text(1:54, mean_correct_mat$`100m`, labels=mean_correct_mat$country, cex= 0.7, pos=1)
-
-plot(x=1:54,y=mean_correct_mat$`200m`,xlab = "Country",ylab="200m")
-text(1:54, mean_correct_mat$`200m`, labels=mean_correct_mat$country, cex= 0.7, pos=1)
-
-plot(x=1:54,y=mean_correct_mat$`400m`,xlab = "Country",ylab="400m")
-text(1:54, mean_correct_mat$`400m`, labels=mean_correct_mat$country, cex= 0.7, pos=1)
-
-plot(x=1:54,y=mean_correct_mat$`800m`,xlab = "Country",ylab="800m")
-text(1:54, mean_correct_mat$`800m`, labels=mean_correct_mat$country, cex= 0.7, pos=1)
-
-plot(x=1:54,y=mean_correct_mat$`1500m`,xlab = "Country",ylab="1500m")
-text(1:54, mean_correct_mat$`1500m`, labels=mean_correct_mat$country, cex= 0.7, pos=1)
-
-plot(x=1:54,y=mean_correct_mat$`3000m`,xlab = "Country",ylab="3000m")
-text(1:54, mean_correct_mat$`3000m`, labels=mean_correct_mat$country, cex= 0.7, pos=1)
-
-plot(x=1:54,y=mean_correct_mat$Marathon,xlab = "Country",ylab="Marathon")
-text(1:54, mean_correct_mat$Marathon, labels=mean_correct_mat$country, cex= 0.7, pos=1)
+for(i in 1:ncol(mean_correct_mat2[,-8])){
+ a<-plot(x=1:nrow(mean_correct_mat2),y=mean_correct_mat2[,i],
+         xlab = "Country",ylab=colnames(mean_correct_mat2)[i])
+  text(1:nrow(mean_correct_mat2), mean_correct_mat2[,i], labels=mean_correct_mat2$country, cex= 0.7, pos=1)
+  print(a)
+}
 
 
-#Q.3.B
 
+#Q.3.D
 
+#Solution given by the in-built R-function "mahalanobis"
+mahalanobis(data[,-1],mean_vect,cov_mat)
+
+#Solution by matrix operations
+d2values<-vector()
+for(i in 1:nrow(data)){
+  d2values[i]<-t(mean_correct_mat[i,])%*%solve(cov_mat)%*%mean_correct_mat[i,]
+}
+d2values
+
+d2frame<-data.frame("Country"=data$Country,d2values)
+d2frame<-d2frame[order(d2values,decreasing = TRUE),]
+head(d2frame)
