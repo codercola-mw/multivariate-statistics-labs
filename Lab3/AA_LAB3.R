f922d58bebfec49566fc8f743b719ea76ae604c1
ahmetakdeve <55084294+ahmetakdeve@users.noreply.github.com>
2019-12-05 17:24:52 +0100

    Assignment 1

--- Lab3/AA_LAB3.R
+++ Lab3/AA_LAB3.R
@@ -29,8 +29,6 @@ fviz_contrib(my_PCA,choice = "var",axes = 1) #Contribution to PC1
 fviz_contrib(my_PCA,choice = "var",axes = 2) #Contribution to PC2
 #This component should be interpreted as IN THE QUESTION! 
 
-#But from PC1 values from each variable, we can see that the short distance races have negative values
-
 ###### D.
 scores_frame<-data.frame("Country"=my_data$Country,"Scores"=my_PCA$x[,1])
 scores_frame<-scores_frame[order(scores_frame$Scores,decreasing = TRUE),]
@@ -39,14 +37,5 @@ head(scores_frame) #Seems reasonable
 
 ############-----------QUESTION 2----------------##############
          
-###### A.
-S_matrix<-cov(my_data[,-1])
-my_fact<-factanal(my_data[,-1],2,covmat = S_matrix)
-#88.6 % of the variance is explanied by those 2 factors
 
-factanal(S_matrix,2,covmat=S_matrix)
-
-factanal(factor=2,covmat = S_matrix)
-
-my_fact$correlation
 
