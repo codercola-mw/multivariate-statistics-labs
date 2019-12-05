c45b486bc0eb7af67b498456df934b1fffe110cc
Jooyoung Lee <jy.leellen@gmail.com>
2019-12-05 17:28:46 +0100

    assignment 1b

--- Lab3/Assignment1.R
+++ Lab3/Assignment1.R
@@ -15,7 +15,7 @@ eigenvect <- eigen(R)$vectors
 ##b. 
 ###. standardize the variable, get two principal components
 for (i in 1:ncol(data)) {
-  data[,i] <- ((data[,i]-mean(data[,i]))/var(data[,i]))
+  data[,i] <- ((data[,i]-mean(data[,i]))/sqrt(var(data[,i])))
 }
 SIG <- cov(data)
 eigenvalSIG <- eigen(SIG)$values
@@ -24,10 +24,30 @@ eigenvectSIG <- eigen(SIG)$vectors
 
 ###. correlation table with names, cummlative percentage of sample variance by two components
 
+Y1 <- matrix(0,nrow=nrow(data),ncol=1)
+for (i in 1:7) {
+  Y1 <- Y1+(eigenvect[,1][i]*data[,i])
+}
+a <- cor(data, Y1)
+
+Y2 <- matrix(0,nrow=nrow(data),ncol=1)
+for (i in 1:7) {
+  Y2 <- Y2+(eigenvect[,2][i]*data[,i])
+}
+b <- cor(data, Y2)
+
+RES <- matrix(c(a,b), ncol=2, nrow=7)
+colnames(RES) <- c("PC1", "PC2")
+rownames(RES) <- c("z1", "z2", "z3", "z4", "z5", "z6", "z7")
+
+
+
 cumvar <- c()
 for (i in 1:2) {
-  res <- eigenvalSIG[i]/sum(eigenvalSIG)
+  res <- eigenvalSIG[i]/7
   cumvar <- append(cumvar, res)
 }
 
-cumvar <- cumsum(cumvar)
+cumvar <- matrix(cumsum(cumvar), nrow=1)
+colnames(cumvar) <- c("PC1", "PC2")
+rownames(cumvar) <- "Cummlative Explained Variance"
