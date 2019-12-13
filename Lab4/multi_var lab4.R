c0ae35f4a47744b59b70166271259ca503bb237f
Duan Zhixuan <darinstu999@gmail.com>
2019-12-13 13:18:09 +0100

    >.<

--- Lab4/multi_var lab4.R
+++ Lab4/multi_var lab4.R
@@ -66,3 +66,36 @@ sqrt(0.01575231)
 # V_y_2 <- 0.375 * y1 - 0.120 * y2
 
 #### Significance test####
+
+corcoef.test<-function(r, n, p, q, alpha=0.05){
+  m<-length(r)
+  Q<-rep(0, m)
+  lambda <- 1
+  
+  for (k in m:1){
+    lambda<-lambda*(1-r[k]^2); 
+    Q[k]<- -log(lambda) 
+  }
+  
+  s<-0; i<-m 
+  
+  for (k in 1:m){
+  }
+  
+  s<-0; i<-m 
+  
+  for (k in 1:m){ 
+    Q[k]<- (n-k+1-1/2*(p+q+3)+s)*Q[k]
+    chi<-1-pchisq(Q[k],(p-k+1)*(q-k+1))
+    if (chi>alpha){
+      i<-k-1
+      break
+    }
+    s<-s+1/r[k]^2
+  }
+  i
+}
+
+corcoef.test(r=sqrt(m2e$values),n=46,p=3,q=2)
+
+# so we choose the first pair of can_corr
